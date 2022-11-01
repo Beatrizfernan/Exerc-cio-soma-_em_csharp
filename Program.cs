16f2fcd8cf3cfc5616641c85a755c0d248ff1fc2
Beatriz Fernandes <93793289+Beatrizfernan@users.noreply.github.com>
2022-10-31 21:08:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercícios_em_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            Console.WriteLine("Digite o número A");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número B");
+            int B = int.Parse(Console.ReadLine());
+
+            int soma;
+            soma = A + B;
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
